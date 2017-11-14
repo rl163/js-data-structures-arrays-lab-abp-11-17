4c3791c7724ce8d96d1fd80ead81109add4d4390
Ryan Lee <ryanlee91@gmail.com>
2017-11-14 05:19:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 // Write your solution here!
 const drivers = ["Milo","Otis","Garfield"];
-
+//appends a driver to the end of the drivers array
 function destructivelyAppendDriver (name){
   drivers.push(name);
 }
-
+//prepends a driver to the beginning of the drivers array
 function destructivelyPrependDriver (name) {
   drivers.unshift(name);
 }
-
+//removes the last driver from the drivers array
 function destructivelyRemoveLastDriver() {
   drivers.pop()
 }
-
+//removes the first driver from drivers array
 function destructivelyRemoveFirstDriver(){
   drivers.shift()
 }
-
+//appends a driver to the drivers array and returns a new array, leaving the drivers array unchanged
 function appendDriver(name){
   return [...drivers,name];
 }
-
