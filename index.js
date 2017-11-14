59e698c3ce1057a8e5b2766a90e1a2e0f565ae32
Ryan Lee <ryanlee91@gmail.com>
2017-11-14 05:15:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function destructivelyAppendDriver (name){
 function destructivelyPrependDriver (name) {
   drivers.unshift(name);
 }
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop()
+}
+
+function
